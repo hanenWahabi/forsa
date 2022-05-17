35a7803bb42efbe15a988bce894b06691d86fa00
Hanen Ouahabi <hanenouahabii@gmail.com>
2022-05-17 20:42:08 +0100

    test2 fork

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-//testing
+//testing2
 const routes: Routes = [
   {path:'',component:LandingPageComponent},
   {path: 'login',component:RegisterComponent}
